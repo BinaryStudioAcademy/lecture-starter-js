9abb114d1ea68bfa75c65f94b63ed608e04893a7
nikita-remeslov <nikita.remeslov@binary-studio.com>
2023-06-06 16:33:11 +0200

    * entry point

--- index.js
+++
@@ -1,4 +0,0 @@
-import App from './src/javascript/app';
-import './src/styles/styles.css';
-
-App.startApp();
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import App from './javascript/app';
+import './styles/styles.css';
+
+App.startApp();
