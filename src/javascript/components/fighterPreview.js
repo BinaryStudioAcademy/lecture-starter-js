f60f5a9af650c50c33ab9708c4dc619d58aecf11
rega1237 <rega1237@gmail.com>
2024-06-04 17:20:28 -0400

    + figtherPreview pain the fighter in the dom with the info

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -1,5 +1,59 @@
 import createElement from '../helpers/domHelper';
 
+export function createFighterImage(fighter, position = null) {
+    const { source, name } = fighter;
+    const attributes = {
+        src: source,
+        title: name,
+        alt: name
+    };
+    const imgElement = createElement({
+        tagName: 'img',
+        className: position > 0 ? 'fighter-preview___img fighter-preview__mirror-img' : 'fighter-preview___img',
+        attributes
+    });
+
+    return imgElement;
+}
+
+function displayFighterInfo(fighters, container) {
+    fighters.forEach((fighter, index) => {
+        if (!fighter) return;
+        const { name, health, attack, defense } = fighter;
+        const imageElement = createFighterImage(fighter, index);
+        const dataContainer = createElement({
+            tagName: 'div',
+            className: 'fighter-preview___data-container'
+        });
+        const nameElement = createElement({
+            tagName: 'span',
+            className: 'fighter-preview___name',
+            attributes: { title: name }
+        });
+        const healthElement = createElement({
+            tagName: 'span',
+            className: 'fighter-preview___health'
+        });
+        const attackElement = createElement({
+            tagName: 'span',
+            className: 'fighter-preview___attack'
+        });
+        const defenseElement = createElement({
+            tagName: 'span',
+            className: 'fighter-preview___defense'
+        });
+
+        nameElement.innerText = `Name: ${name}`;
+        healthElement.innerText = `Health: ${health}`;
+        attackElement.innerText = `Attack: ${attack}`;
+        defenseElement.innerText = `Defense: ${defense}`;
+
+        dataContainer.append(nameElement, healthElement, attackElement, defenseElement);
+
+        container.append(imageElement, dataContainer);
+    });
+}
+
 export function createFighterPreview(fighter, position) {
     const positionClassName = position === 'right' ? 'fighter-preview___right' : 'fighter-preview___left';
     const fighterElement = createElement({
@@ -9,21 +63,11 @@ export function createFighterPreview(fighter, position) {
 
     // todo: show fighter info (image, name, health, etc.)
 
+    const fighterOne = position === 'left' ? fighter : undefined;
+    const fighterTwo = position === 'right' ? fighter : undefined;
+    const fighters = [fighterOne, fighterTwo];
+
+    displayFighterInfo(fighters, fighterElement);
+
     return fighterElement;
-}
-
-export function createFighterImage(fighter) {
-    const { source, name } = fighter;
-    const attributes = {
-        src: source,
-        title: name,
-        alt: name
-    };
-    const imgElement = createElement({
-        tagName: 'img',
-        className: 'fighter-preview___img',
-        attributes
-    });
-
-    return imgElement;
 }
