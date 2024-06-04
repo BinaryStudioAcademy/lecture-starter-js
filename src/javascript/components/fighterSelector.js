a786aabe02fad01072b11b10a215d2f44f0551fb
rega1237 <rega1237@gmail.com>
2024-06-04 16:42:31 -0400

    + fighterSelector.js getFigtherInfo add functionality to get from map or fill the map

--- src/javascript/components/fighterSelector.js
+++ src/javascript/components/fighterSelector.js
@@ -2,11 +2,18 @@ import createElement from '../helpers/domHelper';
 import renderArena from './arena';
 import versusImg from '../../../resources/versus.png';
 import { createFighterPreview } from './fighterPreview';
+import fighterService from '../services/fightersService';
 
 const fighterDetailsMap = new Map();
 
 export async function getFighterInfo(fighterId) {
     // get fighter info from fighterDetailsMap or from service and write it to fighterDetailsMap
+    if (!fighterDetailsMap.has(fighterId)) {
+        const response = await fighterService.getFighterDetails(fighterId);
+        fighterDetailsMap.set(fighterId, response);
+    }
+
+    return fighterDetailsMap.get(fighterId);
 }
 
 function startFight(selectedFighters) {
