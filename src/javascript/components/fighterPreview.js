a5e1521bfb89e688d779adb814cbe7228ddf08fb
Guillermo Martos <guille.l.martos@gmail.com>
2024-06-06 02:01:41 +0200

    - figher details service ready - fighters preview mvp ready

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -8,7 +8,12 @@ export function createFighterPreview(fighter, position) {
     });
 
     // todo: show fighter info (image, name, health, etc.)
-
+    if (fighter) {
+        const previewFighterDivWrapper = createElement({ tagName: 'div', className: 'preview_figther_wrap' });
+        // eslint-disable-next-line no-use-before-define
+        previewFighterDivWrapper.append(...createFighterPreviewData(fighter));
+        fighterElement.append(previewFighterDivWrapper);
+    }
     return fighterElement;
 }
 
@@ -26,4 +31,18 @@ export function createFighterImage(fighter) {
     });
 
     return imgElement;
+}
+
+function createFighterPreviewData(fighter) {
+    const fighterImg = createFighterImage(fighter);
+    const fighterName = createElement({ tagName: 'h3', className: 'name_preview-span' });
+    fighterName.innerText = `${fighter.name}`;
+    const fighterHealth = createElement({ tagName: 'span', className: 'health-span' });
+    fighterHealth.innerText = `Health: ${fighter.health}`;
+    const fighterAttack = createElement({ tagName: 'span', className: 'attack-span' });
+    fighterAttack.innerText = `Attack: ${fighter.attack}`;
+    const fighterDefense = createElement({ tagName: 'span', className: 'defense-span' });
+    fighterDefense.innerText = `Defense: ${fighter.defense}`;
+
+    return [fighterImg, fighterName, fighterHealth, fighterAttack, fighterDefense];
 }
--- src/javascript/services/fightersService.js
+++ src/javascript/services/fightersService.js
@@ -3,6 +3,8 @@ import callApi from '../helpers/apiHelper';
 class FighterService {
     #endpoint = 'fighters.json';
 
+    #fighterDetailsEndpoint = 'details/fighter/REPLACE_ID.json';
+
     async getFighters() {
         try {
             const apiResult = await callApi(this.#endpoint);
@@ -15,6 +17,12 @@ class FighterService {
     async getFighterDetails(id) {
         // todo: implement this method
         // endpoint - `details/fighter/${id}.json`;
+        try {
+            const apiResult = await callApi(this.#fighterDetailsEndpoint.replace('REPLACE_ID', id));
+            return apiResult;
+        } catch (error) {
+            throw error;
+        }
     }
 }
 
