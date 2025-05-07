0349f123984ae64793ee827f68f65eb7218ed82c
tania-macheieva <macheieva.tetiana@gmail.com>
2025-05-07 23:48:50 +0300

    + fighter info on click

--- src/javascript/components/fighterSelector.js
+++ src/javascript/components/fighterSelector.js
@@ -2,11 +2,15 @@ import createElement from '../helpers/domHelper';
 import renderArena from './arena';
 import versusImg from '../../../resources/versus.png';
 import { createFighterPreview } from './fighterPreview';
+import fighterService from '../services/fightersService';
 
 const fighterDetailsMap = new Map();
 
 export async function getFighterInfo(fighterId) {
     // get fighter info from fighterDetailsMap or from service and write it to fighterDetailsMap
+    const details = await fighterService.getFighterDetails(fighterId);
+    fighterDetailsMap.set(fighterId, details);
+    return details;
 }
 
 function startFight(selectedFighters) {
